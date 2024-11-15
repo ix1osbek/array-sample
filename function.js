a8744c21f01bf8709271426a569dfff46ad67e5e
ix1osbek <bekerkinov2004@gmail.com>
2024-11-15 20:19:49 +0500

    function examle

---
+++ function.js
@@ -0,0 +1,80 @@
+
+// function myFunction(num1 , num2) {
+//   return num1+num2
+
+// }
+// console.log(myFunction(10,20));
+// console.log(myFunction(40,30));
+// const result = myFunction(30,40)
+// console.log(result + myFunction(10,20));
+
+// let a = "Salom dunyo"
+// function myFunction(a) {
+
+
+// }
+// console.log(myFunction(a));
+
+/////////  hello world chiqaruvchi funksiya 
+// function goWorld() {
+//   console.log('hello world');
+
+
+// }
+// goWorld ()
+
+/////////// checkAge misol 
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     console.log("Siz haydovchilik guvoxnomasini olishingiz mumkun!");
+
+//   } else {
+//     console.log("Sizning yoshingiz haydovchilik guvoxnomasini olishga to'lmagan🤷‍♂️");
+
+//   }
+
+// }
+// // console.log(checkAge(13));
+// checkAge(20)
+
+
+//////////////  2-misol
+
+// function checkAge() {
+//   let age = prompt("Iltimos yoshingizni kitriting!")
+//   if (age >= 18 && age < 140) {
+//     console.log("Siz chet el safariga chiqishingiz mumkun");
+
+
+//   } else if (age < 18 && age > 0) {
+//     console.log("Siz hali voyaga yetmaganingiz sababli chiqishingiz mumkun emas😢");
+
+
+//   } else {
+//     console.log("Mavjud bo'lmagan son kiritilgan :)");
+
+//   }
+// }
+// checkAge()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////  misoll------------------------
+
+ 
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
