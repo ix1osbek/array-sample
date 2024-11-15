c67293280b348a8cd8978c03132d4ad37b77235c
ix1osbek <bekerkinov2004@gmail.com>
2024-11-15 20:11:21 +0500

    Array masalalari

---
+++ array.js
@@ -0,0 +1,50 @@
+// ////////////// array bu - bir vaqtning ozida bir nechta qiymatlarni saqlashi mumkun bolgan objectga aytiladi!
+
+
+// //// arraylarni oddiy ozgaruvchini elon qilgandek elon qilamiz (pastda)
+// const languages = [ 'js' , 'java' , 'go'];
+
+
+// /// JS  da arreylarni 'new' kalit sozidan ham foydalanib yaratishimiz mumkun(pastda)
+// const languages = new Array ('js', 'java', 'go');
+
+// //////////////// array lar turli hil qiymatlarni ozida saqlashi mumkun (pastda misol)
+
+
+// / string array ----------
+// const languages = [ 'js' , 'java' , 'go'];
+// /// number array ------
+// const number = [2, 3, 4, -2];
+// /// bo'sh array---------
+// const boshArray = [],
+// //// aralash qiymatlardan tashkil topgan array ---------
+// const aralashArray =m ['js', 3, null , false];
+
+
+
+// //////////// array elementlarini olish uchun uning index laridan foydalanib olishimiz mumkun (pastda)
+
+// const helloArray = ['H', 'E', 'L', 'L', 'O'];
+// console.log(helloArray[4]);
+
+
+// ////////////// array ga element qoshish =>  push() yoki unshift()  orqali qoshiladi
+ 
+// / push() orqali qoshish--------------------- push() orqadan qoshadi 
+// const languages = ['js', 'java']
+//  languages.push('go')
+// console.log(languages);
+
+// / unshift() orqali qoshish ---------------- unshift() oldidan qoshadi
+// const languages = ['js', 'java']
+//  languages.unshift('go')
+// console.log(languages);
+
+
+
+// ///////// array elementlarini ozgartirish 
+
+// const languages = [ 'go', 'java'];
+// languages[1] = 'javascript'
+// // console.log(languages.length);
+
